4ec8de0ca45b30008542c16df76cc305a5a71b7a
Enzo Batista <batista.enzo.be@gmail.com>
2023-11-15 15:24:18 +0100

    add boucle changement img ! problème boucle text!

--- assets/script.js
+++ assets/script.js
@@ -35,14 +35,87 @@ for (let i = 0; i < slides.length; i++) {
 	}
 }
 
+
+// Functions
+
+//* Rend l'élément sélectionné avec les dots *//
+
+function addDotToSelectedElement(element) {
+	element.classList.add("dot_selected");
+}
+
+//* Retire le précédent élément sélectionné avec les dots *//
+
+function removeDotFromUnselectedElement(element) {
+	element.classList.remove("dot_selected");
+}
+
+//* On change l'image et le texte *//
+
+function changeSlide(bannerP,nextSlide) {
+	bannerImgName = nextSlide.image;
+	bannerP.innerHTML = nextSlide.text;
+}
+
+function changeImgSrc(imgSrc) {
+	bannerImg.src = "./assets/images/slideshow/" + imgSrc.image;
+}
+
 // Events
 
 let arrowLeft = document.querySelector(".arrow_left");
 		arrowLeft.addEventListener('click', function() {
-			console.log("Vous avez cliqué sur la fleche gauche");
+
+		let index = 0;
+		let bannerP = document.querySelector("#banner p");
+		let dot = document.getElementsByClassName("dot");
+
+		for (let i = 0; i < slides.length; i++) {
+
+			if(bannerImgName === slides[0].image) {
+
+			changeImgSrc(slides[slides.length - 1]);
+			index = slides.length - 1;
+
+			addDotToSelectedElement (dot[index]);
+			removeDotFromUnselectedElement(dot[0]);
+
+		}	else if(bannerImgName === slides[i].image) {
+
+				changeImgSrc(slides[i - 1]);
+				addDotToSelectedElement(dot[i - 1]);
+				removeDotFromUnselectedElement(dot[i]);
+				index = i - 1;
+			}
+	}
+		changeSlide(bannerP, slides[index]);
+
 });
 
 let arrowRight = document.querySelector(".arrow_right");
 		arrowRight.addEventListener('click', function() {
-			console.log("Vous avez cliqué sur la fleche droite");
+
+		let index = 0;
+		let bannerP = document.querySelector("#banner p");
+		let dot = document.getElementsByClassName("dot");
+	
+		for (let i = 0; i < slides.length; i++) {
+
+			if(bannerImgName === slides[slides.length - 1].image) {
+
+			changeImgSrc(slides[0]);
+			addDotToSelectedElement(dot[0]);
+			removeDotFromUnselectedElement(dot[slides.length - 1]);
+
+		} 	else if(bannerImgName === slides[i].image) {
+
+			changeImgSrc(slides[i + 1]);
+			index = i + 1;
+
+			addDotToSelectedElement(dot[index]);
+			removeDotFromUnselectedElement(dot[i]);
+		} 
+	}
+		changeSlide(bannerP, slides[index]);
+
 });
